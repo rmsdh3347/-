4037ed1b0df9be2e40d0736bdd01972bfd70e809
rmsdh3347 <32675540+rmsdh3347@users.noreply.github.com>
2017-10-11 11:03:57 +0900

    Delete 201401258산업경영공학과(야) 김근오 프로그래밍언어 과제.py

--- 201401258산업경영공학과(야) 김근오 프로그래밍언어 과제.py
+++
@@ -1,5 +0,0 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-==================== RESTART: C:/Users/user/Desktop/도형.py ====================
->>> 
