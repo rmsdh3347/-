b4d0c9d2b53958b03bb3023921315ae55005a4cd
rmsdh3347 <32675540+rmsdh3347@users.noreply.github.com>
2017-10-11 11:12:55 +0900

    Add files via upload

---
+++ day9_hw.py
@@ -0,0 +1,15 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle as t
+>>> angle=93
+>>> t.bgcolor("black")
+>>> t.color("blue")
+>>> t.speed(0)
+>>> for x in range(100):
+	t.fd(x)
+	t.rt(angle)
+	t.lt(21)
+	t.circle(x)
+
+	
+>>> 
