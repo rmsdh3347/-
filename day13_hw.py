07b77274101a2d2316a8f43ddf8947b558853801
rmsdh3347 <32675540+rmsdh3347@users.noreply.github.com>
2017-10-29 20:29:13 +0900

    Add files via upload

---
+++ day13_hw.py
@@ -0,0 +1,38 @@
+import turtle as t
+t.shape("turtle")
+t.up() 
+t.fd(250)
+t.lt(90)
+t.fd(250)
+t.lt(90)
+t.down()
+for x in range(4):
+	t.fd(500)
+	t.lt(90)
+
+	
+t.up()
+t.home()
+t.down()
+import random
+a=random.randint(0,359)
+t.seth(a)
+while True:
+	x=t.xcor()
+	y=t.ycor()
+	ang=t.heading()
+	if -245<x<245 and -245<y<245:
+		t.fd(10)
+	else:
+		if x>=245:
+			t.seth(180-ang)
+			t.fd(10)
+		if y>=245:
+			t.seth(-ang)
+			t.fd(10)
+		if x<=-245:
+			t.seth(180-ang)
+			t.fd(10)
+		if y<=-245:
+			t.seth(-ang)
+			t.fd(10)
